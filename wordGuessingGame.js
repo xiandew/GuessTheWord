88d6baa52095e5d5a39b407ffd7ed0eee206514d
Xiande Wen <visiande1998@gmail.com>
2019-03-19 14:15:48 +1100

    Add words.js and setWord()

--- wordGuessingGame.js
+++ wordGuessingGame.js
@@ -3,9 +3,20 @@ let gameState = {
     remainingGuesses: 10
 };
 
-function setWord(word) {
+function setWord() {
     //set word to be guessed
+    let word = words[Math.floor(Math.random() * words.length)].toLowerCase();
+    let hiddenLetters = [];
+    for (let i = 0, numHidden = Math.floor(word.length / 2); i < numHidden; i++) {
+        hiddenLetters.push(Math.floor(Math.random() * word.length));
+    }
+    let wordTiles = "";
+    for (let letter in word) {
+        wordTiles += hiddenLetters.includes(parseInt(letter)) ? `<div class="guess"></div>` : `<div class="char">${word[letter]}</div>`;
+    }
+    document.getElementById("wordTiles").innerHTML = wordTiles;
 }
+
 function validate (guess) {
     //validate the input from the form (letter)
 }
